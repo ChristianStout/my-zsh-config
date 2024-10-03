5e5056a34a4b2a89ad2b21c91c4b0399217fe062
Christian Stout <lambda@san.thisismadeup.com.edu.org.central>
2024-10-02 22:23:07 -0600

    fixed the install script

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 dir=$(pwd)
-rm -r "$dir"/plugins
-cd "$dir"/plugins
+rm -rf plugins/*
+cd plugins
 
-delcare -a plugins=(
-	"https://github.com/romkatv/powerlevel10k.git"
-	"https://github.com/zsh-users/zsh-autosuggestions.git"
-	"https://github.com/zsh-users/zsh-history-substring-search.git"
-	"https://github.com/zsh-users/zsh-syntax-highlighting.git"
-)
+git clone https://github.com/romkatv/powerlevel10k.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-for plugin in ${plugins[@]}
-do
-	git clone "$plugin"
-done
